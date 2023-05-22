4ebcfe4e9f993ebb74232af973259bf12e087cf7
Luca <predictable.fake.name@gmail.com>
2023-05-22 17:05:44 +0200

    Creato relazione con db in Ordine

--- src/main/java/it/corso/model/Ordine.java
+++ src/main/java/it/corso/model/Ordine.java
@@ -1,12 +1,19 @@
 package it.corso.model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -25,5 +32,65 @@ public class Ordine {
 
     @Column(name = "id_utente")
     private int id_utente;
+
+    public int getId() {
+	return id;
+    }
+
+    @ManyToOne(cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "id_utente", referencedColumnName = "id")
+    private Utente utente;
+    
+    @ManyToMany(cascade = CascadeType.REFRESH)
+    @JoinTable(
+	    name = "ordini_prodotti",
+	    joinColumns = @JoinColumn(name = "id_ordine", referencedColumnName = "id"),
+	    inverseJoinColumns = @JoinColumn(name = "id_prodotto", referencedColumnName = "id"))
+    private List<Prodotto> prodotti = new ArrayList<>();
+
+    // Qui iniziano i getter/setter
+    public void setId(int id) {
+	this.id = id;
+    }
+
+    public LocalDate getData() {
+	return data;
+    }
+
+    public void setData(LocalDate data) {
+	this.data = data;
+    }
+
+    public double getImporto() {
+	return importo;
+    }
+
+    public void setImporto(double importo) {
+	this.importo = importo;
+    }
+
+    public int getId_utente() {
+	return id_utente;
+    }
+
+    public void setId_utente(int id_utente) {
+	this.id_utente = id_utente;
+    }
+
+    public Utente getUtente() {
+	return utente;
+    }
+
+    public void setUtente(Utente utente) {
+	this.utente = utente;
+    }
+
+    public List<Prodotto> getProdotti() {
+	return prodotti;
+    }
+
+    public void setProdotti(List<Prodotto> prodotti) {
+	this.prodotti = prodotti;
+    }
 
 }
