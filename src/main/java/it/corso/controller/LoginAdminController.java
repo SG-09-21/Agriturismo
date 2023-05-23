7b64b6d12fe593377aa7b2d4a7e16b204fc9ba9a
Luca <predictable.fake.name@gmail.com>
2023-05-23 11:14:08 +0200

    Added Controllers

--- src/main/java/it/corso/controller/LoginAdminController.java
+++ src/main/java/it/corso/controller/LoginAdminController.java
@@ -1,5 +1,17 @@
 package it.corso.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/loginAdmin")
 public class LoginAdminController {
+
+    @GetMapping
+    private String getPage() {
+
+	return "loginAdmin";
+    }
 
 }
--- src/main/java/it/corso/controller/LoginController.java
+++ src/main/java/it/corso/controller/LoginController.java
@@ -1,16 +1,31 @@
 package it.corso.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import it.corso.service.UtenteService;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
 
-    @GetMapping
-    public String getPage() {
+    @Autowired
+    private UtenteService utenteService;
 
+    @GetMapping
+    public String getPage(@RequestParam(name = "le", required = false) String logError, Model model,
+	    HttpSession session) {
+
+	if (session.getAttribute("utente") != null) {
+	    return "redirect:/reserved";
+	}
+
+	model.addAttribute("logError", logError != null);
 	return "login";
     }
 }
