f45b010b860def0c753fe45021d1b0740b74de28
SaddyGengar <jonathan.ortu@protonmail.com>
2023-05-23 11:13:30 +0200

    loginAdmin

--- src/main/java/it/corso/controller/LoginAdminController.java
+++ src/main/java/it/corso/controller/LoginAdminController.java
@@ -1,5 +1,42 @@
 package it.corso.controller;
 
-public class LoginAdminController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import it.corso.service.UtenteService;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/LoginAdmin")
+public class LoginAdminController {
+	
+	@Autowired
+	UtenteService utenteService;
+	
+	@GetMapping
+	public String getPage(@RequestParam(name="le", required= false) String logError, Model model, HttpSession session) 
+		{
+			if(session.getAttribute("utente")!=null)
+				return "redirect:/reserved";
+			model.addAttribute("logError", logError != null);
+			return "LoginAdmin";
+		}
+	
+	@PostMapping
+	public String getioneLogin(
+			@RequestParam("username") String username,
+			@RequestParam("password") String password,
+			HttpSession session) 
+	{
+		if(!utenteService.controlloLogin(session,username,password)) {
+			return "redirect:/login?le";
+		}
+		
+		return "redirect:/reserved";
+	}
 }
