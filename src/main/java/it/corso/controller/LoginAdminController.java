97368c47fb4a9e89900b199583c64c57f9caee4f
SaddyGengar <jonathan.ortu@protonmail.com>
2023-05-23 15:02:17 +0200

    nuova homepage

--- src/main/java/it/corso/controller/LoginAdminController.java
+++ src/main/java/it/corso/controller/LoginAdminController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -26,12 +25,5 @@ public class LoginAdminController {
 			model.addAttribute("logError", logError != null);
 			return "LoginAdmin";
 		}
-	
-	@PostMapping
-	public String gestioneLogin() {
-		
-		return null;
-	}
-	
 	
 }
