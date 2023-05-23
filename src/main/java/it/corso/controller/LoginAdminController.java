377854fe760bbce5803529983ecf114a9a7f51a1
Luca <predictable.fake.name@gmail.com>
2023-05-23 15:31:31 +0200

    Fix conflicts

--- src/main/java/it/corso/controller/LoginAdminController.java
+++ src/main/java/it/corso/controller/LoginAdminController.java
@@ -1,15 +1,22 @@
 package it.corso.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import it.corso.service.UtenteService;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/loginAdmin")
 public class LoginAdminController {
 	
 	@Autowired
-	UtenteService utenteService;
+	private UtenteService utenteService;
 	
 	@GetMapping
 	public String getPage(@RequestParam(name="le", required= false) String logError, Model model, HttpSession session) 
