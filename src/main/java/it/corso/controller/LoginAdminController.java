a00dd9bcd5e3d4dc51d7b0197e9d8fdcb0321b20
felicemanzi <felice.manzi@outlook.com>
2023-05-24 13:08:29 +0200

    Deleted unused import in class LoginAdminController

--- src/main/java/it/corso/controller/LoginAdminController.java
+++ src/main/java/it/corso/controller/LoginAdminController.java
@@ -3,17 +3,13 @@ package it.corso.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import it.corso.model.Admin;
-import it.corso.model.Utente;
 import it.corso.service.AdminService;
-import it.corso.service.UtenteService;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
