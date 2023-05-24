14d96ce1fd1601c93f64003f47ec5b68d4471052
felicemanzi <felice.manzi@outlook.com>
2023-05-24 13:11:01 +0200

    Deleted some unused Imports

--- src/main/java/it/corso/service/AdminService.java
+++ src/main/java/it/corso/service/AdminService.java
@@ -2,9 +2,7 @@ package it.corso.service;
 
 import org.springframework.stereotype.Service;
 
-import it.corso.model.Admin;
 import jakarta.servlet.http.HttpSession;
-import jakarta.websocket.Session;
 
 @Service
 public interface AdminService {
