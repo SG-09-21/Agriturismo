e276b5aca9d2d1e538da79f69ee0b54246fdc464
Luca <predictable.fake.name@gmail.com>
2023-05-31 23:55:14 +0200

    Add "aggiungi al carrello to catalogo"
    Add toggle show/hide password from area utente
    Fix fallback image

--- src/main/resources/static/javaScript/index.js
+++ src/main/resources/static/javaScript/index.js
@@ -10,3 +10,23 @@ const toggleBtn = document.querySelector('.toggle_btn')
            ? 'fa-solid fa-xmark'
            : 'fa-solid fa-bars'
         }
+        
+// Get references to the necessary elements
+let passwordHidden = document.getElementById("passwordHidden");
+let showPasswordButton = document.getElementById("showPasswordButton");
+let showPw = document.getElementById("showPw");
+
+// Add a click event listener to the button
+showPasswordButton.addEventListener("click", function() {
+  // Toggle the visibility of the password output element
+  if (showPw.style.display === "none") {
+    showPw.style.display = "block";
+    passwordHidden.style.display = "none";
+    
+  } else {
+	  
+	  showPw.style.display = "none";
+	  passwordHidden.style.display = "block";
+  }
+  
+});
